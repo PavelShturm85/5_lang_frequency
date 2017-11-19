31f2eca630297b00fd2ce0f2d67c45035a131ab0
PavelShturm85 <pavel.shturm@gmail.com>
2017-11-19 20:48:46 +0400

    Вывод 10ти часто встречающихся слов из текста.

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+**This module insert on terminal the most frequent world.**
+
+# Quickstart
+**Ways to use:**
+- Have to use  module `lang_frequency.py` after `python3` with argument `name_file.txt` in terminal.
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python3 lang_frequency.py <name_file.txt>
+Самые встречающиеся слова:
+---------------------------
+1.	 и 	    x 1688
+2.	 я 	    x 996
+3.	 в 	    x 860
+4.	 не 	  x 785
+5.	 на 	  x 718
+6.	 или 	  x 591
+7.	 тебя   x 541
+8.	 что 	  x 467
+9.	 с 	    x 342
+10.	 меня   x 269
+
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,41 @@
+import sys
+import re
+import string
+import collections
 
 
-def load_data(filepath):
-    pass
+def load_data(enter_file):
+    with open(enter_file, 'r') as text:
+        content = text.read()
+    return content
 
 
-def get_most_frequent_words(text):
-    pass
+def special_characters(input_text):
+    regexp = ''.format(string.printable)
+    return re.sub(regexp, '', input_text)
+
+
+def get_most_frequent_words(without_special_symbol):
+    counter = collections.Counter()
+    for word in without_special_symbol.split():
+        counter[word] += 1
+    return counter
+
+
+def print_top_words(word_dict):
+    print('Самые встречающиеся слова:')
+    print('---------------------------')
+    place_counter = 1
+    for i in word_dict.most_common(10):
+        print(str(place_counter) + '.' + '\t', i[0], '\t x', i[1])
+        place_counter += 1
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        input_file_name = sys.argv[1]
+    else:
+        input_file_name = input('Введите путь к файлу: ')
+    enter_text = load_data(input_file_name)
+    most_often_words = get_most_frequent_words(special_characters(enter_text))
+    print_top_words(most_often_words)
